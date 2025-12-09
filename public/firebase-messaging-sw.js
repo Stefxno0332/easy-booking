487d4c7325fe53fc43710e19830ff68686a01b2f
Stefano Carbone <stefanocarbone30@gmail.com>
2025-12-09 18:04:20 +0100

    Risolti problemi con le notifiche

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,14 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-/* UTILIZZO LE NOTIFICHE STANDARD DI FIREBASE PER NON RICOPIARE TUTTI I DATI DI FIREBASE
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png',
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+firebase.initializeApp({
+    apiKey: "AIzaSyCS9AXx0PL6liU5OkSjekInjSosLZtDZfQ",
+    authDomain: "easy-booking-52608.firebaseapp.com",
+    projectId: "easy-booking-52608",
+    storageBucket: "easy-booking-52608.firebasestorage.app",
+    messagingSenderId: "317305194326",
+    appId: "1:317305194326:web:440f0482eb6890212438f7"
 });
-*/
+
+const messaging = firebase.messaging();
+// Firebase gestisce automaticamente le notifiche in background con i dati standard
